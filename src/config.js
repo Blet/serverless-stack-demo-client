40c88b726896fc0b7ed8bd97fa5fa54a988a363c
Jay <jvelayud@gmail.com>
2017-07-12 15:04:37 -0400

    Updating to IAM

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://w5z1hloaz5.execute-api.us-east-1.amazonaws.com/test',
+    REGION: 'us-east-1',
+  },
   cognito: {
+    REGION: 'us-east-1',
+    IDENTITY_POOL_ID: 'us-east-1:565cc505-5486-4278-bf0f-9736b9fdd8c5',
     USER_POOL_ID : 'us-east-1_YEhdMFvix',
     APP_CLIENT_ID : '29qpcqorti0ctjkur5h01dph5g',
   }
